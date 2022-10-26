35eb5c5794d2efcfc5a9bfd52440f45e1fd6d996
uvindu <uvinduperera97@gmail.com>
2022-10-26 09:32:09 +0530

    react event handling

--- project2/project2/src/App.js
+++ project2/project2/src/App.js
@@ -3,14 +3,30 @@ import './App.css';
 import { UserProfile } from './UserProfile';
 
 function App() {
+  // const handleOkClick= () =>{
+  //   console.log("Pressed Ok Button")
+  // }
+  const handleCancelClick= () =>{
+    console.log("Pressed Cancel Button")
+  }
+  const handleInputChanger=(event,name,age,obj)=>{
+    // console.log("input value changed")
+    console.log(event.target.value,name,age,obj)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-<UserProfile/>
+        </p> */}
+
+<button onClick={()=>{
+  console.log("click ok")
+}}>OK</button>
+<button onClick={handleCancelClick}>Cacnel</button>
+
+<input type="text" onChange={(event)=>handleInputChanger(event,"Uvindu",24,{})}/>
       </header>
     </div>
   );
