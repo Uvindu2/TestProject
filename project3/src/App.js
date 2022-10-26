a38cedbba67baa97a1249b12a4269666c45d0e61
uvindu <uvinduperera97@gmail.com>
2022-10-26 22:48:04 +0530

    use state hook

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,22 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [user,setUser]=React.useState({
+
+    firstName:'Uvindu',
+    lastName:'Perera'
+  })
+  useEffect(()=>{
+    console.log('Your First Name is :'+user.firstName);
+
+    // console.log('Your Name is : ${user.firstName}');
+    // alert('Your Name is : ${(user.firstName)}')
+
+  },[user.lastName])
+
+
+
+  const handleOnSubmit=()=>{
+    setUser({
+     ...user,
+      lastName:Math.random()
+
+    })
+
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+{user.lastName}
+
+
+<button onClick={handleOnSubmit}>Submit</button>
+
       </header>
     </div>
   );
