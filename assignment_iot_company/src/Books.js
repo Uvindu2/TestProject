5a8af3adf2abd95b56444dd2b1d0a7d82ce6f141
uvindu <uvinduperera97@gmail.com>
2022-10-29 13:20:12 +0530

    fix

--- assignment_iot_company/src/Books.js
+++ assignment_iot_company/src/Books.js
@@ -1,61 +1,73 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {FiExternalLink} from "react"
+
+
 
 
 const Books = () => {
     const [books, setBook] = React.useState([]);
 
     useEffect(() => {
-        const fetchBooks = async () => {
-            const res = await axios.get(
-                `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${process.env.REACT_APP_BOOKS_API_KEY}`
-            );
-            console.log(res.data.results.books)
-            setBook(res.data.results.books)
-        };
-        fetchBooks()
+
+        // Make a request for a user with a given ID
+axios.get('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=euiwTw9ktCGpH1PoRDb4YI9sm8iP8vWS')
+.then(function (response) {
+
+  console.log(response);
+  setBook(response.data.results.books)
+  
+})
+.catch(function (error) {
+  // handle error
+  console.log(error);
+
+
+})
+.finally(function () {
+  // always executed
+
+});
+
+
     }, [])
 
     return (
+        
         <div className="bg-gray-500">
         <>
-            <h1 className="font-bold text-center text-4x1 py-5 lg:text-6xl mb-20 bg-gray-500 text-white	" >Best Selling Books</h1>
-            <section className="grid grid-cols-1 gap-10 px-5 sm:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 bg-gray-500 pb-20"  >
-                {books.map((book) => {
-                    const {author, book_image, buy_links, description, primary_isbn10, publisher, rank, title } = book
-
-                    return (
-                        <article key={rank} class='bg-gray-100 py-5 px-10 rounded-lg sm:px-5 '>
+            <h1 className="font-bold text-center text-4x1 py-5 lg:text-6xl mb-20 text-white	" >Best Selling Books</h1>
+            <section className="grid grid-cols-1 gap-10 px-5 sm:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4  pb-20"  >
+               
+                    
+                    
+                    {books.map((book)=>(<article class='bg-gray-100 py-5 px-10 rounded-lg sm:px-5 '>
                             <div>
-                                <img src={book_image} alt={title} className="block mx-auto w-1/2" />
+                                <img src={book.book_image} alt={book.title} className="block mx-auto w-1/2" />
+                             
                             </div>
                             <div>
-                                <h3 className="font-bold my-2 text-2x1">{title}</h3>
-                                <p className="mb-4">{description}</p>
-                                <p><span className="font-bold">Author: </span>{author}</p>
+                                <h3 className="font-bold my-2 text-2x1">{book.title}</h3>
+                                <p className="mb-4">{book.description}</p>
+                                <p><span className="font-bold">Author: </span>{book.author}</p>
                             </div>
                             <ul className="mb-4">
-                                <li><span className="font-bold">Publicher:</span> {publisher}</li>
-                                <li><span className="font-bold">ISBN: </span>{primary_isbn10}</li>
+                                {/* <li><span className="font-bold">Publicher:</span> {publisher}</li>
+                                <li><span className="font-bold">ISBN: </span>{primary_isbn10}</li> */}
                                                     
                             </ul>
                             <ul >
-                                <p><h3 className="font-bold text-xl">Buy Now :</h3></p>
-                               {buy_links.map((link)=>{
-                               const {name,url}= link
-                               return(
-                                <div key={name}>
-                                   <a href={url}>
-                                    {name}</a>
-                                </div>
-                               )
-
-                               })}
+                               
+                                <a href={book.buy_links[0].url} class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Buy Now </a>
+                              
+                               
                             </ul>
                         </article>
-                    )
-                })}
+                        ))}
+                      
+                        
+                        
+                    
+                
             </section>
 
         </>
